eed210f71904177d11d434231b8f629b831fad67
Василий Карабцов <karabcovas@mail.ru>
2023-07-18 00:06:27 +0300

    func_big_str+func_title+docstring

--- str_func.py
+++ str_func.py
@@ -1,3 +1,10 @@
 def strr():
+    """Делает все буквы заглавными"""
     value = input().upper()
+    return value
+
+
+def strr_title():
+    """Делает первые буквы заглавными"""
+    value = input().title()
     return value
